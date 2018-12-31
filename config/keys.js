ef74f049e08bde032893ab67d128c3194dc64a3d
Fantosism <businessspamfolder@gmail.com>
2018-12-31 09:41:40 -0700

    :tada:, initial dependencies :pushpin: scripts and db uri

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://HelloIAmAdmin:1337horsebatterystapler@ds145704.mlab.com:45704/mymdb"
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 8000; // Heroku's port uses process.env.PORT
+
+app.listen(port, () => console.log(`Server is connected and listening on port ${port} !`));
